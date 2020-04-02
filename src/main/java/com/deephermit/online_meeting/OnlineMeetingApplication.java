9a46371c0fe062ff84c8ea49a40b2da946152863
DeepHermit <343506776@qq.com>
2020-04-02 20:51:36 +0800

    create project include spring boot DevTools,lombok,spring web,spring web services,JDBC API,mysql driver,mybatis framework

---
+++ src/main/java/com/deephermit/online_meeting/OnlineMeetingApplication.java
@@ -0,0 +1,13 @@
+package com.deephermit.online_meeting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineMeetingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OnlineMeetingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/deephermit/online_meeting/OnlineMeetingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.deephermit.online_meeting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlineMeetingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
